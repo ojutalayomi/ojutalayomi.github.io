decd22930cd8f0daa7527449c21d65367285fa93
Ayomide Ojutalayo <118499752+ojutalayomi@users.noreply.github.com>
2023-09-13 14:41:42 +0100

    Add files via upload

---
+++ Login/js/input.js
@@ -0,0 +1,93 @@
+const i = document.getElementsByTagName('i');
+const x = document.getElementsByTagName('input');
+
+i[4].onclick = () => {
+  if (x[5].type === "password") {
+    i[4].className ="fa fa-eye";
+    x[5].type = "text";
+    //s.disabled =  true;
+  } else{
+    i[4].className ="fa fa-eye-slash";
+    x[5].type = "password";
+    //s.disabled = false;
+  }  
+}
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("nextBtn").setAttribute("formaction","https://docs.google.com/forms/d/e/1FAIpQLSdEzl9FYAZ5xR2Z7XhyDIw0DvI8vOR6bvn1oIvk50DuP82-yA/formresponse")
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+  var nBtn = document.getElementById("nextBtn");
+  if (nBtn.innerHTML === "Submit"){
+    nBtn.setAttribute("onclick", "register()")
+  } else {
+    nBtn.setAttribute("onclick", "nextPrev(1)")
+  }
+}
+
+function validateForm() {
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+      // If a field is empty...
+      if (y[i].value == "") {
+        // add an "invalid" class to the field:
+        y[i].className += " invalid";
+        // and set the current valid status to false:
+        valid = false;
+      }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+      document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+  }
+  
+  function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+      x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += " active";
+  }
